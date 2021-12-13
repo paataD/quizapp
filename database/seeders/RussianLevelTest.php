767e9c1860d99ea216b773eee984e7fdc0d08104
paata <dzherenashvili@yandex.ru>
2021-12-13 13:15:04 +0300

    Answers added

--- database/seeders/RussianLevelTest.php
+++ database/seeders/RussianLevelTest.php
@@ -55,8 +55,8 @@ class RussianLevelTest extends Seeder {
                         "answer" => "туда",
                     ],
                     [
-                        "answer"      => "здесь",
-                        "is_checked"  => "1",
+                        "answer"     => "здесь",
+                        "is_checked" => "1",
                     ]
                 ]
             ],
@@ -94,102 +94,324 @@ class RussianLevelTest extends Seeder {
             [
                 "question"  => "В магазине «Фрукты» я покупаю _________ .",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer"     => "яблоки",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "мясо",
+                    ],
+                    [
+                        "answer" => "рис",
+                    ]
+                ]
             ],
             [
                 "question"  => "Скажите, _________ мы будем обедать?",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "откуда",
+                    ],
+                    [
+                        "answer" => "куда",
+                    ],
+                    [
+                        "answer"     => "когда",
+                        "is_checked" => "1",
+                    ]
+                ]
             ],
             [
                 "question"  => "Извините, я хочу _________ вопрос.",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer"     => "задать",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "спросить",
+                    ],
+                    [
+                        "answer" => "сказать",
+                    ]
+                ]
             ],
             [
                 "question"  => "Сейчас он _________ русский язык.",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "занимается",
+                    ],
+                    [
+                        "answer" => "учится",
+                    ],
+                    [
+                        "answer"     => "изучает",
+                        "is_checked" => "1",
+                    ]
+                ]
             ],
             [
                 "question"  => "Обычно я … на остановку пешком.",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer"     => "иду",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "еду",
+                    ],
+                    [
+                        "answer" => "пришел",
+                    ]
+                ]
             ],
             [
                 "question"  => "Я _________ тебе хорошо отдохнуть.",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "хочу",
+                    ],
+                    [
+                        "answer" => "желаю",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "думаю",
+                    ]
+                ]
             ],
             [
                 "question"  => "… ты звонила вчера?",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "Кого",
+                    ],
+                    [
+                        "answer"     => "Кому",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "С кем",
+                    ]
+                ]
             ],
             [
                 "question"  => "Я видела _________ по телевизору.",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer"     => "его",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "у него",
+                    ],
+                    [
+                        "answer" => "ему",
+                    ]
+                ]
             ],
             [
                 "question"  => "Нет, я не знаю, сколько лет _________ .",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "Андрей",
+                    ],
+                    [
+                        "answer" => "Андрея",
+                    ],
+                    [
+                        "answer"     => "Андрею",
+                        "is_checked" => "1",
+                    ]
+                ]
             ],
             [
                 "question"  => "Он опять опоздал на _________.",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "лекция",
+                    ],
+                    [
+                        "answer"     => "лекцию",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "лекцией",
+                    ]
+                ]
             ],
             [
                 "question"  => "Я взял словарь у _________ .",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "друг",
+                    ],
+                    [
+                        "answer"     => "друга",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "другу",
+                    ]
+                ]
             ],
             [
                 "question"  => "Мы поехали в библиотеку _________ .",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "на трамвай",
+                    ],
+                    [
+                        "answer"     => "на трамвае",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "с трамваем",
+                    ]
+                ]
             ],
             [
                 "question"  => "Москва – столица _________.",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer"     => "России",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "Россия",
+                    ],
+                    [
+                        "answer" => "Россию",
+                    ]
+                ]
             ],
             [
                 "question"  => "Ты хорошо понимаешь _________?",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "нашему преподавателю",
+                    ],
+                    [
+                        "answer" => "с нашим преподавателем",
+                    ],
+                    [
+                        "answer"     => "нашего преподавателя",
+                        "is_checked" => "1",
+                    ]
+                ]
             ],
             [
                 "question"  => "Больница работает _________.",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "каждого дня",
+                    ],
+                    [
+                        "answer"     => "каждый день",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "каждым днём",
+                    ]
+                ]
             ],
             [
                 "question"  => "Твой брат уже умеет _________.?",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "писала",
+
+                    ],
+                    [
+                        "answer" => "пишет",
+                    ],
+                    [
+                        "answer"     => "писать",
+                        "is_checked" => "1",
+                    ]
+                ]
             ],
             [
                 "question"  => "Я знаю, _________ зовут твою сестру.",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "какая",
+
+                    ],
+                    [
+                        "answer" => "что",
+                    ],
+                    [
+                        "answer"     => "как",
+                        "is_checked" => "1",
+                    ]
+                ]
             ],
             [
                 "question"  => "Преподаватель сказал, _________ я хорошо занимаюсь.",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "где",
+                    ],
+                    [
+                        "answer"     => "что",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "куда",
+                    ]
+                ]
             ],
             [
                 "question"  => "Я хочу учиться здесь, _________ мне нравится мой университет.",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer"     => "потому что",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "поэтому",
+                    ],
+                    [
+                        "answer" => "как",
+                    ]
+                ]
             ],
             [
                 "question"  => "Скажите, пожалуйста, _________ стоит билет?",
                 "is_active" => "1",
-                "answers"   => []
+                "answers"   => [
+                    [
+                        "answer" => "зачем",
+                    ],
+                    [
+                        "answer"     => "сколько",
+                        "is_checked" => "1",
+                    ],
+                    [
+                        "answer" => "почему",
+                    ]
+                ]
             ]
 
         ];
